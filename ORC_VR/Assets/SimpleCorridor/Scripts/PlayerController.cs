cd6625158c3c4fcc2ec70930a443c219b4af1ff4
namazu1028 <45709526+namazu1028@users.noreply.github.com>
2019-02-02 19:10:11 +0900

    豚転がしVR

---
+++ ORC_VR/Assets/SimpleCorridor/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public SteamVR_Input_Sources hand;
+    public SteamVR_Action_Boolean grabAction;
+
+    public GameObject cameraRig;
+    public GameObject controllerR;
+
+    private Vector3 cameraRigStartPosition;
+    private Vector3 controllerRStartPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grabAction.GetStateDown(hand))
+        {
+            controllerRStartPosition = controllerR.transform.localPosition;
+            cameraRigStartPosition = cameraRig.transform.position;
+            Debug.Log(controllerRStartPosition);
+            Debug.Log("GrabDown!");
+        }
+        if (grabAction.GetState(hand) && !grabAction.GetStateDown(hand))
+        {
+            var diff = controllerR.transform.localPosition - controllerRStartPosition;
+            Debug.Log(diff);
+            // diff = new Vector3(0, -0.1f, 0);
+            cameraRig.transform.position = -diff + cameraRigStartPosition;
+        }
+        if (grabAction.GetStateUp(hand))
+        {
+            Debug.Log("GrabUp!");
+        }
+    }
+}
